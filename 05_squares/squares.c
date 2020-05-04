ab5ff5b0e99c3bdfabd03576aa4969d98abb4961
Coursera Learner <nobody@nowhere.nul>
2020-05-04 18:38:28 +0000

    Did assignment 05

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -7,8 +7,9 @@
  * offset (INCLUSIVE) and offset + size (EXCLUSIVE)
  */
 int isInRange(int coord, int offset, int size) {
-  // if coord is in range, return 1
-  // else, return 0
+  if(coord>=offset&&coord<size)
+    return 1;
+  else// if coord is in range, return 1 // else, return 0
   return 0;
 }
 
@@ -17,14 +18,41 @@ int isInRange(int coord, int offset, int size) {
  * offset + size
  */
 int isAtBorder(int coord, int offset, int size) {
-  // if coord is equal to offest or offset + size
-  // return 1, else return 0
-  return 0;
+  if(coord==offset||coord==(offset+size)) // if coord is equal to offest or offset + size
+    return 1;
+  else {// return 1, else return 0
+    return 0;}
 }
 
-void squares(int size1, int x_offset, int y_offset, int size2) {
-  //compute the max of size1 and (x_offset + size2).  Call this w
-
+void squares(int size1, int x_offset, int y_offset, int size2) 
+{
+  int w,h;
+  if(size1>(x_offset+size2))  //compute the max of size1 and (x_offset + size2).  Cis w
+    w=size1;
+  else
+    w=x_offset+size2;
+  if(size1>(y_offset+size2))
+     h=size1;
+     else
+       h=y_offset+size2;
+     for(int y=0;y<=h;y++)
+       {
+	 for(int x=0;x<=w;x++)
+	   {
+	     if(((x>=x_offset&&x<(x_offset+size2))&&(y==y_offset||y==(y_offset+size2-1)))||((y>=y_offset&&y<(y_offset+size2))&&(x==x_offset||x==(x_offset+size2-1))))
+	       printf("*");
+	     else
+	       {
+		 if((x<size1&&(y==0||y==(size1-1)))||(y<size1&&(x==0||x==(size1-1))))
+		   printf("#");
+		 else
+		   printf(" ");
+	       }
+	  
+	   }
+	 printf("\n");
+       }
+}
   //compute the max of size1 and (y_offset + size2).  Call this h
 
   //count from 0 to h. Call the number you count with y
@@ -50,4 +78,4 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     //when you finish counting x from 0 to w, 
     //print a newline
 
-}
+
